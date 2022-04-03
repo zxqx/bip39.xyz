9c5eebf6c3166b72ceffc9c23ddeaedc6d4c604d
Zak Angelle <zaknotzak@gmail.com>
2022-04-02 20:24:29 -0500

    resolve lint warnings

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 declare module 'bip39/bip39';
 declare module 'toasty';
+declare module 'react-use-dimensions';
--- src/components/AudioRecorder.tsx
+++ src/components/AudioRecorder.tsx
@@ -1,8 +1,6 @@
 import { useState, useMemo, useCallback } from 'react';
 import { entropyToMnemonic } from 'bip39/bip39';
 import { sha256 } from 'js-sha256';
-import zxcvbn from 'zxcvbn';
-// @ts-ignore
 import useDimensions from 'react-use-dimensions';
 import { useSpring, animated } from 'react-spring';
 import AudioRecorder, { RecordState } from '../../lib/audio-react-recorder/dist/index.modern';
@@ -24,7 +22,6 @@ export default () => {
     RecordState.STOP
   );
   const [mnemonic, setMnemonic] = useState('');
-  const [timeToCrackDisplay, setTimeToCrackDisplay] = useState<number | string>('');
   const [isProcessing, setIsProcessing] = useState(false);
 
   const startRecording = useCallback(() => {
@@ -42,9 +39,6 @@ export default () => {
     const entropyInput = new Int32Array(audioData).slice(20);
     const entropy = sha256(entropyInput);
     const mnemonic = entropyToMnemonic(entropy);
-    const entropyStrength = zxcvbn(entropy);
-    setTimeToCrackDisplay(entropyStrength.crack_times_display.offline_fast_hashing_1e10_per_second);
-    console.log(timeToCrackDisplay);
     setMnemonic(mnemonic);
     setIsProcessing(false);
   }, []);
@@ -61,7 +55,11 @@ export default () => {
 
   const animationProps = useSpring(renderAnimation);
 
-  const isWaveformVisible = useMemo(() => (isRecording || isProcessing) && !mnemonic, [isRecording, isProcessing, !mnemonic]);
+  const isWaveformVisible = useMemo(() => (isRecording || isProcessing) && !mnemonic, [
+    isRecording,
+    isProcessing,
+    !mnemonic,
+  ]);
 
   const microphoneErrorAnimationProps = useSpring({
     opacity: hasMicrophoneError ? 1 : 0,
